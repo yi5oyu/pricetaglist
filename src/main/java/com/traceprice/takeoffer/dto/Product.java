9804c33177e245c3e5d112412200dab3500f30a5
Young <yi5oyu@naver.com>
2024-04-17 01:55:25 +0900

    DTO 수정(Product)

--- src/main/java/com/traceprice/takeoffer/dto/Product.java
+++ src/main/java/com/traceprice/takeoffer/dto/Product.java
@@ -3,6 +3,8 @@ package com.traceprice.takeoffer.dto;
 import io.netty.util.internal.IntegerHolder;
 import lombok.*;
 
+import java.sql.Date;
+
 
 @Getter
 @Setter
@@ -13,9 +15,11 @@ import lombok.*;
 public class Product {
     private String market_name;
     private String product_type;
-    private int product_number;
+    private Long product_number;
     private String pname;
     private int fixed_price;
     private String item_img;
-    private int item_number;
+    private Long item_number;
+    private Date price_date;
+    private int daliy_price;
 }
