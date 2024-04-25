3a9df769a765e7c9167a95f05e770e8d6cd70383
Young <yi5oyu@naver.com>
2024-04-26 00:36:57 +0900

    mysql 설정 / ViewRepository 수정

--- src/main/java/com/traceprice/takeoffer/Repository/ViewRepository.java
+++ src/main/java/com/traceprice/takeoffer/Repository/ViewRepository.java
@@ -1,8 +1,9 @@
 package com.traceprice.takeoffer.Repository;
 
+import com.traceprice.takeoffer.entity.View;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ViewRepository extends JpaRepository {
+public interface ViewRepository extends JpaRepository<View, Long> {
 }
