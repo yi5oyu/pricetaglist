6a07d6cacff6aeb5dfcce87e20a1ef45330cbac5
Young <yi5oyu@naver.com>
2024-05-03 02:49:29 +0900

    repository 메소드 추가

--- src/main/java/com/traceprice/takeoffer/Repository/DeliveryRepository.java
+++ src/main/java/com/traceprice/takeoffer/Repository/DeliveryRepository.java
@@ -1,9 +1,13 @@
 package com.traceprice.takeoffer.Repository;
 
 import com.traceprice.takeoffer.entity.Delivery;
+import com.traceprice.takeoffer.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DeliveryRepository extends JpaRepository<Delivery, Long> {
+    Optional<Delivery> findByItemId(Long itemId);
 }
