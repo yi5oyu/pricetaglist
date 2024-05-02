c73fde2b9ad317490ec1adab1c11026ad1a4db19
Young <yi5oyu@gmail.com>
2024-05-03 03:03:43 +0900

    메소드 추가(id, date and id)

--- src/main/java/com/traceprice/takeoffer/Repository/ProductInfoByDateRepository.java
+++ src/main/java/com/traceprice/takeoffer/Repository/ProductInfoByDateRepository.java
@@ -4,6 +4,15 @@ import com.traceprice.takeoffer.entity.ProductInfoByDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProductInfoByDateRepository extends JpaRepository<ProductInfoByDate, Long> {
+
+    List<ProductInfoByDate> findByItemId(Long itemId);
+    boolean existsByItemId(Long itemId);
+
+    Long countByPriceDateAndItemId(Date priceDate, Long itemId);
 }
