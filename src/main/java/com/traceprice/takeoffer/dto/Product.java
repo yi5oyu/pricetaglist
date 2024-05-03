f082fb1b671b4c8a4bf7434b8e2172f295f8ad3e
Young <yi5oyu@gmail.com>
2024-05-03 16:16:12 +0900

    Product(DTO) 필드(deliveryFee) 추가

--- src/main/java/com/traceprice/takeoffer/dto/Product.java
+++ src/main/java/com/traceprice/takeoffer/dto/Product.java
@@ -26,6 +26,7 @@ public class Product {
     private int discountRate;
     private String detailInfo;
     private String itemQuantity;
+    private int deliveryFee;
 
     private String address;
 }
