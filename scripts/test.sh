6c98ddc261d59a57146d0b4155a2494c6c65acb1
Young <yi5oyu@gmail.com>
2024-05-25 05:45:48 +0900

    pem 복호화 test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 echo "test success"
-echo "$EC2_INSTANCE_IP"
-echo "$DOCKER_USERNAME"
-openssl aes-256-cbc -d -pbkdf2 -k "$ENCRYPTED_PASSWORD" -in scripts/pricetaglist.pem.enc -out scripts/deploy_key.pem
-chmod 400 scripts/deploy_key.pem
+
+openssl aes-256-cbc -d -k "$ENCRYPTED_PASSWORD" -in scripts/pricetaglist.pem.enc -out scripts/deploy_key.pem
+# 해독이 성공하면 파일 권한 설정
+if [ -f scripts/deploy_key.pem ]; then
+  chmod 400 scripts/deploy_key.pem
+  echo "ssh 진입"
+  # SSH 접속 (실제로 사용하려면 주석 해제)
+  # ssh -o StrictHostKeyChecking=no ec2-user@$EC2_INSTANCE_IP
+else
+  echo "Failed to decrypt the file."
+fi
 #ssh -o StrictHostKeyChecking=no ec2-user@$EC2_INSTANCE_IP
-echo "ssh 진입"
+echo "fin"
