1c679cffae5f1dee72b8ab0a1566c6a8596beacb
Young <yi5oyu@gmail.com>
2024-05-25 08:43:06 +0900

    scripts test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "test success"
-  ssh -i scripts/pricetaglist.pem -o StrictHostKeyChecking=no ec2-user@$EC2_INSTANCE_IP
+  ssh -i pricetaglist.pem -o StrictHostKeyChecking=no ec2-user@$EC2_INSTANCE_IP
 echo "fin"
