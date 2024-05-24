e60da22d302aa50234e95ddbd6c211ea528840c4
Young <yi5oyu@gmail.com>
2024-05-25 07:55:01 +0900

    scripts test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "test success"
-  - docker build -t yi5oyu/pricetaglist .
-  - echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  - docker push yi5oyu/pricetaglist
+  docker build -t yi5oyu/pricetaglist .
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+  docker push yi5oyu/pricetaglist
 echo "fin"
