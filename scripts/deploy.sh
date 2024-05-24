87d08263bc0aa6a034db5cfddaee4b9f42dcab09
Young <yi5oyu@gmail.com>
2024-05-24 22:23:50 +0900

    deploy.sh 스크립트 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,12 @@
 # EC2 인스턴스에 SSH 접속하여 Docker 컨테이너를 업데이트
 # Traivs ci에서 환경변수 설정 필요
 ssh -o StrictHostKeyChecking=no ec2-user@$EC2_INSTANCE_IP << 'EOF'
+echo "Docker Hub 로그인"
 echo \$DOCKER_PASSWORD | docker login -u \$DOCKER_USERNAME --password-stdin
-docker pull $DOCKER_USERNAME/pricetaglist
+echo "Pull Docker image"
+docker pull \$DOCKER_USERNAME/pricetaglist
+echo "Docker containers 종료"
 docker-compose down || true
+echo "Docker containers 시작"
 docker-compose up -d
 EOF
