df1555730c9cbc35a37e9fac1377c5cd984897b8
Young <yi5oyu@gmail.com>
2024-05-25 05:37:10 +0900

    travis 환경변수, pem 복호화 test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 echo "test success"
-ssh -o StrictHostKeyChecking=no ec2-user@$EC2_INSTANCE_IP
+echo "$EC2_INSTANCE_IP"
+echo "$DOCKER_USERNAME"
+openssl aes-256-cbc -d -pbkdf2 -k "$ENCRYPTED_PASSWORD" -in scripts/pricetaglist.pem.enc -out scripts/deploy_key.pem
+chmod 400 scripts/deploy_key.pem
+#ssh -o StrictHostKeyChecking=no ec2-user@$EC2_INSTANCE_IP
 echo "ssh 진입"
