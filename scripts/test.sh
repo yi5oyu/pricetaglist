adbe10b7fb916e316e517b9ffffb8441fa46100f
Young <yi5oyu@gmail.com>
2024-05-25 09:48:37 +0900

    복호화 test

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,14 +4,14 @@ echo "test success"
 # Option 1: 기본 해독 시도 (AES-256-CBC)
 
   # Option 5: 다른 암호화 방식 시도 (AES-256-CFB)
-openssl aes-256-cfb -d -kfile "$ENCRYPTED_PASSWORD" -in pricetaglist.pem.enc -out scripts/deploy_key.pem
-  if [ $? -eq 0 ] && [ -f scripts/deploy_key.pem ]; then
-    chmod 400 scripts/deploy_key.pem
+openssl aes-256-cfb -d -kfile "$ENCRYPTED_PASSWORD" -in pricetaglist.pem.enc -out pricetaglist.pem
+  if [ $? -eq 0 ] && [ -f pricetaglist.pem ]; then
+    chmod 400 pricetaglist.pem
     echo "Option 5: Decrypt success"
-    ssh -i scripts/deploy_key.pem -o StrictHostKeyChecking=no ec2-user@$EC2_INSTANCE_IP
+    ssh -i pricetaglist.pem -o StrictHostKeyChecking=no ec2-user@$EC2_INSTANCE_IP
   else
     echo "Option 5: Failed to decrypt the file."
-    rm -f scripts/deploy_key.pem
+    rm -f pricetaglist.pem
 fi
 
 
