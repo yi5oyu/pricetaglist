9911ee58c8df719edaa9afb776a6b67e6ed230a2
Young <yi5oyu@gmail.com>
2024-05-31 20:45:08 +0900

    html 구조 수정

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -12,25 +12,26 @@ $(document).ready(function() {
                 $(this).find('.card-front').css("display", "block");
             }
         )
+        $('.card').each(function() {
+            let $card = $(this);
+            let hammer = new Hammer(this);
+
+            hammer.on('tap', function(event) {
+                if ($card.hasClass('card-hover')) {
+                    $card.removeClass('card-hover');
+                    $card.removeClass('card-tag');
+                    $card.find('.card-front').css("display", "block");
+                } else {
+                    $card.addClass('card-hover');
+                    $card.find('.card-front').css("display", "none");
+                    $card.addClass('card-tag');
+                }
+            });
+
+            hammer.on('doubletap', function(event) {
+                window.location.href = $('.card-box-a').attr('href')
+            });
+        })
     }
     card_hover()
-    $('.card').each(function() {
-        let $card = $(this);
-        let hammer = new Hammer(this);
-
-        hammer.on('tap', function(event) {
-            if ($card.hasClass('card-hover')) {
-                $card.removeClass('card-hover');
-                $card.removeClass('card-tag');
-                $card.find('.card-front').css("display", "block");
-            } else {
-                $card.addClass('card-hover');
-                $card.find('.card-front').css("display", "none");
-                $card.addClass('card-tag');
-            }
-        });
-
-        hammer.on('doubletap', function(event) {
-            window.location.href = $('.card-box-a').attr('href')
-        });
-})
+}
