cf8709009e4022e9f36a53fafc55254278f60f5f
Young <yi5oyu@gmail.com>
2024-06-12 16:49:50 +0900

    스크립트 수정

--- scripts/mk.sh
+++ scripts/mk.sh
@@ -2,15 +2,13 @@
 
    eval $(ssh-agent -s)
 
-    # 환경 변수에서 Base64로 인코딩된 키를 디코딩하여 .pem 파일 생성
     echo "$EC2_KEY_BASE64" | base64 --decode > my-ec2-key.pem
     chmod 400 my-ec2-key.pem
 
-    # SSH 키를 추가
     ssh-add my-ec2-key.pem
+
     if [ $? -eq 0 ]; then
 
-      # SSH 접속
       ssh -t -o StrictHostKeyChecking=no ec2-user@$EC2_INSTANCE_IP <<EOF
       cd ~/docker
       docker-compose down || true
@@ -18,15 +16,13 @@
       docker rmi -f yi5oyu/nginx
       docker images -f "dangling=true" -q | xargs docker rmi -f
       docker-compose up -d --force-recreate
-      echo "확인"
-      docker-compose ps
+#      docker-compose ps
 EOF
 
     else
       rm -f my-ec2-key.pem
     fi
 
-    # .pem 파일 삭제
     rm -f my-ec2-key.pem
 
-    echo "fin"
+    echo "종료"
